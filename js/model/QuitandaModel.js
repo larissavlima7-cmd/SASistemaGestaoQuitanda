8860823870ae938ea11336420779ef9c753da818
lvlima <larissa.v.lima7@aluno.senai.br>
2026-04-28 22:00:31 -0300

    alteração

--- js/model/QuitandaModel.js
+++ js/model/QuitandaModel.js
@@ -1,44 +1,31 @@
-// js/models/QuitandaModel.js
-
+//tivemos mais dificuladade
 export class QuitandaModel {
     constructor() {
-        // RF-03: Aqui é a nossa "gaveta" de produtos
         this.produtos = []; 
-        
-        // RF-05: Aqui é o nosso "caderninho" de histórico
         this.movimentacoes = [];
     }
 
-    // +addProduto(Produto) -> Atende o Diagrama e o RF-01
     addProduto(novoProduto) {
-        // 1. Guardamos o produto na gaveta
         this.produtos.push(novoProduto);
-        
-        // 2. RN-04: Temos que anotar que ele entrou!
         this.registrarMovimentacao(novoProduto.nome, 'Entrada', novoProduto.quantidade);
     }
 
-    // +sellProduto(Produto, quant) -> Atende o Diagrama e o RF-04
     sellProduto(produtoAlvo, quantidadeParaVender) {
-        // 1. Pedimos para o objeto Produto tentar se vender (diminuir o estoque dele)
         const vendeuComSucesso = produtoAlvo.venda(quantidadeParaVender);
         
-        // 2. Se ele conseguiu vender (tinha estoque), anotamos no caderninho
         if (vendeuComSucesso) {
             this.registrarMovimentacao(produtoAlvo.nome, 'Saída', quantidadeParaVender);
             return true;
         }
         
-        return false; // Não vendeu (estoque insuficiente)
+        return false;
     }
 
-    // Método para o Histórico (RF-05)
-    // Este método é quem "escreve" no caderninho de movimentações
     registrarMovimentacao(nome, tipo, qtd) {
         const registro = {
             data: new Date().toLocaleString(),
             nomeProduto: nome,
-            tipo: tipo, // 'Entrada' ou 'Saída'
+            tipo: tipo,
             quantidade: qtd
         };
         this.movimentacoes.push(registro);
