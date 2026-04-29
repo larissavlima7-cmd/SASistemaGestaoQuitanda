b959f033da08105706e97b478f68632510648d02
lvlima <larissa.v.lima7@aluno.senai.br>
2026-04-28 23:01:10 -0300

    28/04/2026

--- js/view/QuitandaView.js
+++ js/view/QuitandaView.js
@@ -1,28 +1,52 @@
 export class QuitandaView {
     constructor() {
         this.tabela = document.getElementById('tabelaEstoque');
+        this.elementoMensagem = document.getElementById('mensagemQuitanda');
     }
 
-    renderEstoque(produtos) {
-       this.tabela.innerHTML = "";
+    renderEstoque(produtos, vender) {
+        
+        this.tabela.innerHTML = "";
 
-        // 2. Para cada produto, criamos uma "linha" (tr) na tabela
+        
         produtos.forEach(p => {
-            const linha = `
-                <tr>
-                    <td>${p.id}</td>
-                    <td>${p.nome}</td>
-                    <td>R$ ${p.preco}</td>
-                    <td>${p.quantidade}</td>
-                </tr>
-            `;
-            // 3. Colocamos a linha dentro da tabela no HTML
-            this.tabela.innerHTML += linha;
+            
+            const linha = document.createElement("tr");
+
+            
+            const tdId = document.createElement("td");
+            tdId.textContent = p.id;
+
+            const tdNome = document.createElement("td");
+            tdNome.textContent = p.nome;
+
+            const tdPreco = document.createElement("td");
+            tdPreco.textContent = `R$ ${p.preco}`;
+
+            const tdQtd = document.createElement("td");
+            tdQtd.textContent = p.quantidade;
+
+          
+            const tdAcoes = document.createElement("td");
+            const btnVender = document.createElement("button");
+            btnVender.textContent = "Vender";
+            
+        
+            btnVender.addEventListener("click", () => vender(p, 1)); 
+
+            tdAcoes.appendChild(btnVender);
+
+            //linhas dentro da tabela
+            linha.appendChild(tdId);
+            linha.appendChild(tdNome);
+            linha.appendChild(tdPreco);
+            linha.appendChild(tdQtd);
+            linha.appendChild(tdAcoes);
+
+            this.tabela.appendChild(linha);
         });
     }
-
-    // +renderVenda()
-    renderVenda(mensagem) {
-        alert(mensagem); // Apenas um aviso para o usuário
+    renderVenda(texto) {
+        this.elementoMensagem.textContent = texto;
     }
 }
